32c7efe9b87f92779148a1ddf70ce91895836424
Michał Milanowski <michalmilanowski@op.pl>
2016-08-11 15:16:52 +0200

    Dodanie alertu gdy hasło jest niewprowadzone lub złe, wyświetlanie wiadomości o błędzie z API

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,6 +8,9 @@ function sendPassword(pass) {
 		url: "https://efigence-camp.herokuapp.com/api/login",
 		error: function (response) {
 			console.log(response.responseText);
+			var alertMessage = jQuery.parseJSON(response.responseText);
+			$('.wrong-password').show();
+			$('.alert-text').html(alertMessage.message);
 		},
 		success: function (response) {
 			console.log("succes", response);
@@ -16,13 +19,21 @@ function sendPassword(pass) {
 }
 
 $(document).on('ready', function () {
+	$('#clear-login').on('click', function () {
+		$('.login-number, .password-input, .wrong-password').hide();
+		$('.user-photo').fadeTo(0, 0);
+		$('.login-number-input, .login-number-tip').show();
+	});
+
 	$('.main-button').on('click', function (event) {
 		event.preventDefault();
 		var password = $('#password').val();
-		if (!password.length) {
-			alert('Wpisz hasło!');
-		} else {
-			sendPassword(password);
+		if ($('#password').is(':visible')) {
+			if (!password.length) {
+				$('.wrong-password').show();
+			} else {
+				sendPassword(password);
+			}
 		}
 	});
 });
