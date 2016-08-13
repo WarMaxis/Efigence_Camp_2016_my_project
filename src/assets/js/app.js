cbbdf3dfac2056017ee710df943322595f6de7c4
Michał Milanowski <michalmilanowski@op.pl>
2016-08-13 12:28:25 +0200

    Usprawnienie interakcji przed wpisaniem loginu, upakowanie części skryptów w funkcje

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,30 @@
+function emptyLogin() {
+	$('.wrong-password').show();
+	$('.alert').css('background-color', '#F08A24');
+	$('.alert-text').html('Wprowadź swój login');
+
+	$('.login-number-input').on('click', function () {
+		$('.wrong-password').hide();
+	});
+}
+
+function goToPassword() {
+	var login = $('.login-number-input').val();
+
+	$('.login-number-tip, .login-number-input').hide();
+	$('.login-number, .password-input, .other-button').show();
+	$('.user-photo').css('visibility', 'visible');
+
+	$('.login-string').html(login);
+	$('.login-number-input').val('');
+}
+
+function clearPassword() {
+	$('.login-number-tip, .login-number-input').show();
+	$('.login-number, .password-input, .other-button').hide();
+	$('.user-photo').css('visibility', 'hidden');
+}
+
 function sendPassword(pass) {
 	$.ajax({
 		type: "post",
@@ -20,28 +47,29 @@ function sendPassword(pass) {
 
 $(document).on('ready', function () {
 	$('#clear-login').on('click', function () {
-
+		clearPassword();
 	});
 
 	$('.main-button').on('click', function (event) {
 		event.preventDefault();
 
-		if ($('.login-number-input').length) {
+		if ($('.login-number-input').is(':visible')) {
 			var login = $('.login-number-input').val();
-			$('.login-number-tip, .login-number-input').hide();
-			$('.login-number, .password-input').show();
-			$('.user-photo').css('visibility', 'visible');
-			$('.login-string').html(login);
+
+			if (login == 0) {
+				emptyLogin();
+			} else {
+				goToPassword();
+			};
 
 		} else {
 			var password = $('#password').val();
-			if ($('#password').is(':visible')) {
-				if (!password.length) {
-					$('.wrong-password').show();
-				} else {
-					sendPassword(password);
-				}
-			}
+
+			if (!password.length) {
+				$('.wrong-password').show();
+			} else {
+				sendPassword(password);
+			};
 		};
 	});
 });
